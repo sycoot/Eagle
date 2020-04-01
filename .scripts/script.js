eb205f5f231c5eaccfe57d96819360c2734f3d10
Albatross <61018127+sycoot@users.noreply.github.com>
2020-04-01 16:19:50 +0200

    Modify navigation menu and add and stylize footer

--- .scripts/script.js
+++ .scripts/script.js
@@ -1,8 +1,25 @@
 function openMenu() {
     let menu = document.querySelector("nav#menu");
+    displayOverlay();
     if(menu.style.right != "0%") {
         menu.style.right = "0%";
     } else {
         menu.style.right = "-100%";
     }
+}
+function displayOverlay() {
+    let overlay = document.querySelector("span#overlay");
+    if(overlay.style.left != "0%") {
+        overlay.style.left = "0%";
+    } else {
+        overlay.style.left = "-100%";
+    }
+}
+let main = document.querySelector("main");
+function scrollToMain() {
+    main.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
+let header = document.querySelector("header");
+function scrollToHeader() {
+    header.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
